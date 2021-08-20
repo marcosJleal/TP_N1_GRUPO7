4d217b16ada20e7a9ce6d4bddf3b069037fe4405
CristianResler15 <63179411+CristianResler15@users.noreply.github.com>
2021-08-20 11:08:25 -0300

    Update Calculadora.java
    agregado de los botones de la calculadora.

--- app/src/main/java/utn/frgp/edu/ar/tpn1/Calculadora.java
+++ app/src/main/java/utn/frgp/edu/ar/tpn1/Calculadora.java
@@ -16,5 +16,113 @@ public class Calculadora extends AppCompatActivity {
         setContentView(R.layout.activity_calculadora);
         this.setTitle(R.string.TituloCalculadora);
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+        this.setTitle(R.string.TituloCalculadora);
+        Resultado=(TextView)findViewById(R.id.txtResultado);
+    }
+
+    public void btnNum0(View vw){
+    float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("0");
+    }
+    else Resultado.setText(Resultado.getText() + "0");
+
+    }
+    @SuppressLint("SetTextI18n")
+    public void btnNum1(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("1");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"1");
+    }
+
+    }
+    public void btnNum2(View vw){
+     float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("2");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"2");
+    }
+
+    }
+    public void btnNum3(View vw){
+    float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("3");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"3");
+    }
+
+    }
+    public void btnNum4(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("4");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"4");
+    }
+
+    }
+    public void btnNum5(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("5");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"5");
+    }
+
+    }
+    public void btnNum6(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("6");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"6");
+    }
+
+    }
+    public void btnNum7(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("7");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"7");
+    }
+
+    }
+    public void btnNum8(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("8");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"0");
+    }
+
+    }
+    public void btnNum9(View vw){
+        float num=Float.parseFloat(Resultado.getText().toString());
+    if(num==0.0f){
+        Resultado.setText("9");
+    }
+    else{
+        Resultado.setText(Resultado.getText()+"9");
+    }
+
+    }
+
 
 }
